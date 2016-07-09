3ce609585c849032ebf28c10f3d32fba65a434c5
goddessting <1766153116@qq.com>
2016-07-09 13:16:04 +0800

    all case is considered commitly

--- main/main.js
+++ main/main.js
@@ -1,9 +1,22 @@
-function getPrice(count){
-  if(count <= 2)
-  {
-    return 6;
-  }
-  else{
-    return (6+0.8*count);
-  }
+function getMileagePrice(mileage) {
+    if (mileage <= 2) {
+        return 6;
+    }
+    else if (mileage >= 2 && mileage <= 8) {
+        return Math.round(6 + 0.8 * mileage);
+    }
+    else {
+        return (6 + mileage);
+    }
+}
+
+function getWaitTimePrice(waitTime){
+
+    return Math.round(waitTime * 0.25);
+}
+
+function getTotalPrice(mileage, waitTime){
+    var mileagePrice = getMileagePrice(mileage);
+    var waitTimePrice = getWaitTimePrice(waitTime);
+    return mileagePrice + waitTimePrice;
 }
--- test/main-test.js
+++ test/main-test.js
@@ -1,21 +1,71 @@
-describe('calculate price', function(){
-  describe('when mileage is not more than 2', function(){
-    var input = 2;
+describe('calculate price', function () {
+    var waitTime;
 
-    it('return right price', function () {
-      expect(getPrice(inputs)).toEqual(6);
-
+    beforeEach(function () {
+        waitTime = 4;
     });
 
-  });
+    describe('calculate price by mileage', function () {
 
-  describe('when mileage is more than', function(){
-    var input = 3;
+        it('when mileage is not more than 2, it should return start price 2rmb', function () {
+            var mileage = 2;
+            var price = getMileagePrice(mileage);
+            expect(price).toEqual(6);
+        });
 
-    it('return right price', function(){
-      expect(getPrice(input)).toEqual(8.4);
+        it('when mileage is more than 2 and not more than 8, it return price 9rmb', function () {
+            var mileage = 3;
+            var price = getMileagePrice(mileage);
+            expect(price).toEqual(8);
+        });
 
-    })
+        it('when mileage is more than 8, it return price 16rmb', function () {
+            var mileage = 9;
+            var price = getMileagePrice(mileage);
+            expect(price).toEqual(15);
+        });
+    });
 
-  });
+    describe('calculate price by waiting time', function () {
+        it('when waiting time is 2 minutes, it should return price 1rmb', function(){
+            var waitTime = 2;
+            var price  = getWaitTimePrice(waitTime);
+            expect(price).toEqual(1);
+        });
+
+        it('when waiting time is 5 minutes, it should return price 1rmb', function(){
+            var waitTime = 5;
+            var price  = getWaitTimePrice(waitTime);
+            expect(price).toEqual(1);
+        });
+
+        it('when waiting time is 10 minutes, it should return price 3rmb', function(){
+            var waitTime = 10;
+            var price  = getWaitTimePrice(waitTime);
+            expect(price).toEqual(3);
+        });
+    });
+
+    describe('calculate total price', function(){
+        it('when mileage is 2 and waiting time is 3, it should return price 7', function(){
+            var mileage = 2;
+            var waitTime = 3;
+            var totalPrice = getTotalPrice(mileage, waitTime);
+            expect(totalPrice).toEqual(7);
+        });
+
+        it('when mileage is 3 and waiting time is 6, it should return price 10', function(){
+            var mileage = 3;
+            var waitTime = 6;
+            var totalPrice = getTotalPrice(mileage, waitTime);
+            expect(totalPrice).toEqual(10);
+        });
+
+        it('when mileage is 9 and waiting time is 11, it should return price 18', function(){
+            var mileage = 9;
+            var waitTime = 11;
+            var totalPrice = getTotalPrice(mileage, waitTime);
+            expect(totalPrice).toEqual(18);
+        });
+    });
 });
